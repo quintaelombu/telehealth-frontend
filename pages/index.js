2f41410d996bfdfff1c7b4504c9e0e939e1a119c
quintaelombu <jegaldeano46@gmail.com>
2025-11-09 19:34:15 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '20%' }}>
+      <h1>Teleconsulta Emilio</h1>
+      <p>Frontend conectado correctamente al backend.</p>
+    </div>
+  );
+}
